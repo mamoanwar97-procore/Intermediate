a90f45f07662d098d60423e7d0f0f4a7d107c00b
Mahmoud3ali <mahmoud.ali@procore.com>
2024-12-10 01:32:07 +0200

    chore: add rebase algorithm

--- python.py
+++ python.py
@@ -1,6 +1,5 @@
 import os
 import xmltodict
-import subprocess
 
 parent_dir = os.getcwd()
 default_collection_branch = 'translations-collection'
@@ -9,13 +8,15 @@ git_token = os.environ['GITHUB_TOKEN']
 git_url = os.environ['GIT_ORIGIN_URL']
 print(git_token)
 print(git_url)
+
 # Parse the XML file
 with open(f'{parent_dir}/reference.xml') as fd:
     doc = xmltodict.parse(fd.read())
 
-subprocess.run(['git', 'remote', 'add', 'origin', git_url])
-subprocess.run(['git', 'checkout', "origin", default_collection_branch])
-subprocess.run(['git', 'config', 'pull.rebase', 'false'])
+os.system(f'git remote add origin {git_url}')
+os.system(f'git checkout origin {default_collection_branch}')
+os.system('git config pull.rebase false')
+
 # Loop over the project elements in the XML file
 for project in doc['manifest']['project']:
     repo = project['@name']
@@ -23,7 +24,7 @@ for project in doc['manifest']['project']:
     branch = project.get('@branch')
     
     # create new branch to collect all translations updates from all repos
-    subprocess.run(['git', 'pull', 'origin', branch])
+    os.system(f'git pull origin {branch}')
     
-subprocess.run(['git', 'push', 'origin', default_collection_branch])
-subprocess.run(['gh', 'pr', 'create', '--base', 'main', '--head', default_collection_branch, '--title', 'Translations Update', '--body', 'Translations Update'])
+os.system(f'git push origin {default_collection_branch}')
+os.system('gh pr create --base main --head translations-collection --title "Translations Update" --body "Translations Update"')

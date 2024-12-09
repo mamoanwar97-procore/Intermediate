1d2dedf3dac941d0c5a98a1e0659873ad650a076
Mahmoud3ali <mahmoud.ali@procore.com>
2024-12-10 01:29:09 +0200

    chore: add rebase algorithm

--- python.py
+++ python.py
@@ -6,16 +6,14 @@ parent_dir = os.getcwd()
 default_collection_branch = 'translations-collection'
 
 git_token = os.environ['GITHUB_TOKEN']
-git_origin = os.environ['GIT_ORIGIN']
 git_url = os.environ['GIT_ORIGIN_URL']
 print(git_token)
-print(git_origin)
 print(git_url)
 # Parse the XML file
 with open(f'{parent_dir}/reference.xml') as fd:
     doc = xmltodict.parse(fd.read())
 
-subprocess.run(['git', 'fetch'])
+subprocess.run(['git', 'remote', 'add', 'origin', git_url])
 subprocess.run(['git', 'checkout', "origin", default_collection_branch])
 subprocess.run(['git', 'config', 'pull.rebase', 'true'])
 # Loop over the project elements in the XML file

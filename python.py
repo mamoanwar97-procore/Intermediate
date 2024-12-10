282cc47c23611ae1ebab19bcc6771d2876bc61a2
Mahmoud3ali <mahmoud.ali@procore.com>
2024-12-10 03:44:50 +0200

    chore: add rebase algorithm

--- python.py
+++ python.py
@@ -11,15 +11,15 @@ with open(f'{parent_dir}/reference.xml') as fd:
 
 subprocess.run(['git', 'fetch', '--all'])
 subprocess.run(['git', 'checkout', "origin", default_collection_branch])
-subprocess.run(['git', 'config', 'pull.rebase', 'false'])
-# Loop over the project elements in the XML file
-for project in doc['manifest']['project']:
-    repo = project['@name']
-    revision = project.get('@revision', 'main') # This is optional, default to 'main'
-    branch = project.get('@branch')
+# subprocess.run(['git', 'config', 'pull.rebase', 'false'])
+# # Loop over the project elements in the XML file
+# for project in doc['manifest']['project']:
+#     repo = project['@name']
+#     revision = project.get('@revision', 'main') # This is optional, default to 'main'
+#     branch = project.get('@branch')
     
-    # create new branch to collect all translations updates from all repos
-    subprocess.run(['git', 'pull', 'origin', branch])
+#     # create new branch to collect all translations updates from all repos
+#     subprocess.run(['git', 'pull', 'origin', branch])
     
-subprocess.run(['git', 'push', 'origin', default_collection_branch])
-# subprocess.run(['gh', 'pr', 'create', '--base', 'main', '--head', default_collection_branch, '--title', 'Translations Update', '--body', 'Translations Update'])
+# subprocess.run(['git', 'push', 'origin', default_collection_branch])
+# # subprocess.run(['gh', 'pr', 'create', '--base', 'main', '--head', default_collection_branch, '--title', 'Translations Update', '--body', 'Translations Update'])

c07fc4ab56c266b09ea6b93bbbb4d10a4d7ad8e6
adel-procore <adel.mohamed@procore.com>
2024-12-10 02:26:50 +0200

    test

--- collectFiles.js
+++ collectFiles.js
@@ -37,6 +37,7 @@ parseString(xmlData, (err, result) => {
 
       // Navigate into the cloned repo
       const fileToCopy = `${repoDir}/${filePath}`;
+      console.log("fileToCopy", fileToCopy);
       const destDir = `${parentDir}/${fileToCopy}`; // Destination folder to store files
 
       // Ensure the destination directory exists

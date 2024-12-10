4fad7428e5b37170c3e176ef108b2ba426c43a24
Mahmoud3ali <mahmoud.ali@procore.com>
2024-12-10 02:45:53 +0200

    chore: add rebase algorithm

---
+++ python.py
@@ -0,0 +1,16 @@
+import os
+import xmltodict
+import subprocess
+
+main_branch = 'main'
+username='mamoanwar97-procore'
+parent_dir = os.getcwd()
+default_collection_branch = 'translations-collection'
+
+# Parse the XML file
+with open(f'{parent_dir}/reference.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+
+#git clone the current repo
+subprocess.run(['git', 'clone', '-b', main_branch, f'git@github.com:{username}/{repo}', f'{parent_dir}'])
+subprocess.run(['ls', '-la', f'{parent_dir}'])

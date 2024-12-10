98cbe90116b48b01e2f3d34384201fcf5700c36b
Mahmoud3ali <mahmoud.ali@procore.com>
2024-12-10 06:00:44 +0200

    chore: add rebase algorithm

--- python.py
+++ python.py
@@ -1,27 +1,16 @@
-import os
-import xmltodict
-import subprocess
-
-parent_dir = os.getcwd()
-default_collection_branch = os.environ.get('BRANCH')
-env = os.environ.copy()
-print(env)
-print(default_collection_branch)
-
-# Parse the XML file
-with open(f'{parent_dir}/reference.xml') as fd:
-    doc = xmltodict.parse(fd.read())
-
-subprocess.run(['git', 'checkout', '-b', default_collection_branch])
-subprocess.run(['git', 'config', 'pull.rebase', 'false'])
-# Loop over the project elements in the XML file
-for project in doc['manifest']['project']:
-    repo = project['@name']
-    revision = project.get('@revision', 'main') # This is optional, default to 'main'
-    branch = project.get('@branch')
+def run():
+    parent_dir = os.environ.get('PWD')
+    default_collection_branch = os.environ.get('BRANCH')
+    env = os.environ.copy()
+    print(env)
+    print(default_collection_branch)
+    subprocess.run(['ls'], cwd=f'{parent_dir}/.git/refs/heads')
+    subprocess.run(['git', 'fetch'], cwd=f'{parent_dir}')
+    subprocess.run(['git', 'checkout', 'main'], cwd=f'{parent_dir}')
+    subprocess.run(['git', 'checkout', '-b', default_collection_branch], cwd=f'{parent_dir}')
+    subprocess.run(['git', 'config', 'pull.rebase', 'false'], cwd=f'{parent_dir}')
+    subprocess.run(['git', 'pull', 'origin', 'update-translations-mfe1'], cwd=f'{parent_dir}')
     
-    # create new branch to collect all translations updates from all repos
-    subprocess.run(['git', 'pull', f'origin/{branch}'])
-    
-# # subprocess.run(['git', 'push', 'origin', default_collection_branch])
-# # # subprocess.run(['gh', 'pr', 'create', '--base', 'main', '--head', default_collection_branch, '--title', 'Translations Update', '--body', 'Translations Update'])
+
+if __name__ == "__main__":
+    run()

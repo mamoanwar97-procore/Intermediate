0667fedeef2f8090506a2eae08bbd2cb79df575a
Mahmoud3ali <mahmoud.ali@procore.com>
2024-12-10 06:04:15 +0200

    chore: add rebase algorithm

--- python.py
+++ python.py
@@ -2,6 +2,9 @@ import os
 import xmltodict
 import subprocess
 
+with open(f'{parent_dir}/reference.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+
 def run():
     parent_dir = os.environ.get('PWD')
     default_collection_branch = os.environ.get('BRANCH')
@@ -13,7 +16,13 @@ def run():
     subprocess.run(['git', 'checkout', 'main'], cwd=f'{parent_dir}')
     subprocess.run(['git', 'checkout', '-b', default_collection_branch], cwd=f'{parent_dir}')
     subprocess.run(['git', 'config', 'pull.rebase', 'false'], cwd=f'{parent_dir}')
-    subprocess.run(['git', 'pull', 'origin', 'update-translations-mfe1'], cwd=f'{parent_dir}')
+    
+    # Loop over the project elements in the XML file
+    for project in doc['manifest']['project']:
+        branch = project.get('@branch')
+    
+        # create new branch to collect all translations updates from all repos
+        subprocess.run(['git', 'pull', 'origin', branch], cwd=f'{parent_dir}')
     
 
 if __name__ == "__main__":

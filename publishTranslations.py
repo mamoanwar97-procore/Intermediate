e786854f35b7f936734614a00528ac5e1a2372e1
adel-procore <adel.mohamed@procore.com>
2024-12-10 15:30:10 +0200

    wip

--- publishTranslations.py
+++ publishTranslations.py
@@ -63,5 +63,6 @@ def run():
     folders = get_folders_in_pr(pr_number, repo_name, repo_owner, github_token)
     logging.info(f"Folders in PR: {folders}")
 
+run()
 # if __name__ == "__main__":
 #     run()

3870506ff1cbd7c312e71debb774a58345ca79f2
mamoanwar97-procore <mahmoud.m.anwar@procore.com>
2024-12-11 01:34:49 +0200

    retry: slack notifications

--- slack_notify.py
+++ slack_notify.py
@@ -1,9 +1,9 @@
-from bot_utils import (
+from slack_utils.bot_utils import (
     group_prs_by_channel,
     send_translation_delivery_message_to_all,
 )
-from use_reference_to_list_repos_slack import use_reference_to_list_repos_slack
-from use_repo_channel_ids_dict_to_list_prs import (
+from slack_utils.use_reference_to_list_repos_slack import use_reference_to_list_repos_slack
+from slack_utils.use_repo_channel_ids_dict_to_list_prs import (
     use_repo_channel_ids_dict_to_list_prs,
 )
 

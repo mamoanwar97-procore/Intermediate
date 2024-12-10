bcf1c96fa897c6132123ca4e62b0d8bfb3c8cee4
adel-procore <adel.mohamed@procore.com>
2024-12-10 16:33:18 +0200

    wip

--- publishTranslations.py
+++ publishTranslations.py
@@ -36,7 +36,7 @@ if isinstance(github_token, bytes):
 
 
 # print all the folders in the PR
-def get_folders_in_pr(pr_number, repo_name, github_token):
+def get_folders_in_pr(pr_number:26, repo_name, github_token):
     # Use the correct GitHub API endpoint for PR details
     pr_url = f"https://api.github.com/repos/{repo_name}/pulls/{pr_number}"
     logging.info(f"PR URL: {pr_url}")
